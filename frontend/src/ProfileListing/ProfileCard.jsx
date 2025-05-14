e994f0d6aa4b4df87875fd0ea6b31be0ae874688
sneha-1404 <sneha.22210052@viit.ac.in>
2025-05-14 23:10:41 +0530

    Profile listing page

--- frontend/src/ProfileListing/ProfileCard.jsx
+++ frontend/src/ProfileListing/ProfileCard.jsx
@@ -1,7 +1,16 @@
+import './ProfileCard.css'
+
 export default function ProfileCard(){
     return(
         <>
-            <h1>Profile card</h1>
+            <div class="card">
+                <img src="" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="/ProfilePage" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </>
     )
 }
